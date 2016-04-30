63cf9e7ae0472164d55697fed929dfb2bda89cc5
anbnyc <alecbarrett@gmail.com>
2016-04-29 21:24:50 -0400

    data location

--- src/ham-app.controller.js
+++ src/ham-app.controller.js
@@ -9,8 +9,8 @@ app.controller('HamiltonController', function($scope, $http, Viz){
     $scope.currentViz = "gantt";
     
     $scope.vizzes = {
-        gantt: new Viz("Soundtrack Chronology", "../data/albumDates.json","In what years did the real life events that this song describes occur?"),
-        matrix: new Viz("Character Co-occurrence","../data/cooccurence.json","How many songs does each character share with each other character?")
+        gantt: new Viz("Soundtrack Chronology", "data/albumDates.json","In what years did the real life events that this song describes occur?"),
+        matrix: new Viz("Character Co-occurrence","data/cooccurence.json","How many songs does each character share with each other character?")
     };
     
     $scope.drilldown = {
